990226af300c4fe55cf73b7ea28c2f47a0558445
joelmuraguri <joelkmuraguri21@gmail.com>
2024-03-05 17:04:41 +0300

    add design model

---
+++ feature/authentication/src/main/java/com/joel/authentication/onboarding/OnboardingScreen.kt
@@ -0,0 +1,12 @@
+package com.joel.authentication.onboarding
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OnboardingScreen() {
+
+    Scaffold {
+
+    }
+}
